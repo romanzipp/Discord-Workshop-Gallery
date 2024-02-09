300de3e84861498f86344fce90c19072ece207f9
Roman Zipp <ich@ich.wtf>
2024-02-09 21:20:32 +0100

    Add awards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import Image from 'next/image';
 import { useSession, signIn, signOut } from 'next-auth/react';
-import { useState, Fragment, useMemo } from 'react';
+import {
+    useState, Fragment, useMemo, useRef, useEffect, createRef,
+} from 'react';
 import { useRouter } from 'next/router';
 import moment from 'moment';
+import classNames from 'classnames';
+import Draggable from 'react-draggable';
 import Layout from '@/components/layout';
 import { Button } from '@/components/ui/button';
 import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
@@ -34,9 +38,236 @@ function LoginPage() {
     );
 }
 
+function findClosestMessage(el) {
+    while (el && el !== document) {
+        if (el.dataset.message) {
+            return el;
+        }
+        el = el.parentNode;
+    }
+    return null;
+}
+
+function GalleryPage({ galleryData }) {
+    // Awards
+
+    const awards = [
+        {
+            id: 'top-1', title: '1.', unique: true, appliedClassName: 'bg-red-950', badgeClassName: 'bg-red-900',
+        },
+        {
+            id: 'top-2', title: '2.', unique: true, appliedClassName: 'bg-orange-950', badgeClassName: 'bg-orange-900',
+        },
+        {
+            id: 'top-3', title: '3.', unique: true, appliedClassName: 'bg-yellow-950', badgeClassName: 'bg-yellow-900',
+        },
+        {
+            id: 'top-4', title: '4.', unique: true, appliedClassName: 'bg-green-950', badgeClassName: 'bg-green-900',
+        },
+        {
+            id: 'top-5', title: '5.', unique: true, appliedClassName: 'bg-blue-950', badgeClassName: 'bg-blue-900',
+        },
+        {
+            id: 'additional', title: '❤️', unique: false, appliedClassName: 'bg-gray-950', badgeClassName: 'bg-gray-900',
+        },
+    ];
+
+    const [itemAwards, setItemAwards] = useState([]);
+
+    function resetAwards() {
+        setItemAwards([]);
+    }
+
+    function addAwardToMessage(messageId, awardId) {
+        console.log('addAwardToMessage', messageId, awardId);
+
+        const award = awards.find((a) => a.id === awardId);
+
+        setItemAwards((prevItemAwards) => {
+            let tmp = [...prevItemAwards];
+
+            if (award.unique) {
+                tmp = tmp.filter((awardItem) => awardItem.award.id !== award.id);
+            }
+
+            const messageAlreadyHasAward = tmp.filter((awardItem) => awardItem.messageId === messageId).length > 0;
+
+            if (messageAlreadyHasAward) {
+                tmp = tmp.filter((awardItem) => awardItem.messageId !== messageId);
+            }
+
+            return [
+                ...tmp,
+                { messageId, award },
+            ];
+        });
+    }
+
+    const awardBadges = useMemo(() => awards.map((award) => ({
+        ...award,
+        used: award.unique && itemAwards.find((a) => a.award.id === award.id),
+    })), [itemAwards]);
+
+    // Drag event
+
+    const [draggingAward, setDraggingAward] = useState(null);
+
+    function onStart(event) {
+        event.dataTransfer.setData('text/plain', event.target.id);
+
+        console.log('DRAG >', 'dragging', event.target.dataset.award);
+
+        setDraggingAward(event.target.dataset.award);
+    }
+
+    // Gallery items
+
+    const onItemAllowDrop = (event) => {
+        event.preventDefault();
+    };
+
+    const onItemDrop = (event) => {
+        event.preventDefault();
+
+        const messageElement = findClosestMessage(event.target);
+        if (!messageElement) {
+            return;
+        }
+
+        const messageId = messageElement?.dataset?.message;
+
+        console.log('DRAG <', 'drop', { message: messageId, award: draggingAward });
+
+        addAwardToMessage(messageId, draggingAward);
+    };
+
+    // Component
+
+    const computedMessages = useMemo(() => galleryData?.data?.map((message) => ({
+        ...message,
+        award: itemAwards.find((itemAward) => itemAward.messageId === message.id)?.award,
+    })), [galleryData, itemAwards, awards]);
+
+    console.log(computedMessages);
+
+    return (
+        <Layout>
+            <div className="fixed right-0 flex h-full w-24 items-center">
+                <div className="w-full rounded-l-md border border-r-0 border-white/30 p-4 shadow-lg shadow-white/10">
+                    <div className="text-center text-sm font-semibold uppercase">
+                        Awards
+                    </div>
+
+                    <div className="mt-4 flex select-none flex-col gap-4">
+                        {awardBadges.map((award) => (
+                            <div
+                                key={award.id}
+                                className="pointer-events-auto relative size-16"
+                            >
+                                <div
+                                    onDragStart={(event) => onStart(event)}
+                                    data-award={award.id}
+                                    draggable="true"
+                                >
+                                    <div className={classNames(
+                                        award.badgeClassName,
+                                        award.used && 'opacity-30',
+                                        'fixed flex size-16 items-center justify-center rounded-full p-6 transition-transform hover:scale-105 cursor-pointer font-bold',
+                                    )}
+                                    >
+                                        {award.title}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <button
+                        type="button"
+                        onClick={() => resetAwards()}
+                        className="mt-4 block w-full text-center text-xs font-semibold uppercase text-white/60 transition-colors hover:text-wrap"
+                    >
+                        Reset
+                    </button>
+
+                </div>
+            </div>
+
+            <div className="grid grid-cols-1 gap-4 p-4 pr-32 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
+                {computedMessages.map((message) => (
+                    <div
+                        key={message.id}
+                        onDragOver={(event) => onItemAllowDrop(event)}
+                        onDrop={(event) => onItemDrop(event)}
+                        data-message={message.id}
+                        className={classNames(
+                            message.award ? [message.award.appliedClassName] : 'hover:bg-white/10',
+                            'relative flex flex-col justify-between gap-4 rounded border border-white/30 p-2 transition-colors hover:shadow-lg hover:shadow-white/10',
+                        )}
+                    >
+                        {message.attachments?.length > 0 && (
+                            <div>
+                                {message.attachments?.filter((attachment, index) => index < 1)?.map((attachment) => (
+                                    <div key={attachment.id}>
+                                        <img
+                                            src={attachment.url}
+                                            alt=""
+                                        />
+                                    </div>
+                                ))}
+                                {message.attachments.length > 1 && (
+                                    <div className="mt-2 text-xs font-medium text-white/60">
+                                        +
+                                        {message.attachments.length - 1}
+                                        {' '}
+                                        more
+                                    </div>
+                                )}
+                            </div>
+                        )}
+
+                        {message.author && (
+                            <div className="flex items-center justify-between gap-2 text-sm font-medium">
+                                <div className="flex items-center gap-2 text-white/90">
+                                    <Image
+                                        src={message.author.avatarURL}
+                                        alt={message.author.username}
+                                        height={24}
+                                        width={24}
+                                        className="rounded-full"
+                                    />
+                                    {message.author.globalName}
+                                </div>
+                                <div className="text-xs text-white/60">
+                                    {moment(message.createdTimestamp).fromNow()}
+                                </div>
+                            </div>
+                        )}
+
+                        {message.award && (
+                            <div className={classNames(
+                                message.award.badgeClassName,
+                                'absolute right-0 top-0 size-12 rounded-full -mr-2 -mt-2 flex items-center justify-center font-bold',
+                            )}
+                            >
+                                {message.award.title}
+                            </div>
+                        )}
+                    </div>
+
+                ))}
+            </div>
+        </Layout>
+    );
+}
+
 export default function Home() {
     const { data: session } = useSession();
     const router = useRouter();
+
+    const [originFromBookmark, setOriginFromBookmark] = useState(!!router.query.channel);
+
+    console.log(originFromBookmark);
 
     const selectedGuild = useMemo(() => router.query.guild, [router]);
     const selectedChannel = useMemo(() => router.query.channel, [router]);
@@ -146,71 +377,5 @@ export default function Home() {
         );
     }
 
-    return (
-        <Layout>
-            <div className="fixed right-0 flex h-full w-24 items-center">
-                <div className="w-full rounded-l-md border border-r-0 border-white/30 p-4 shadow-lg shadow-white/10">
-                    <div className="text-center text-sm font-semibold uppercase">
-                        Awards
-                    </div>
-
-                    <div className="mt-4 flex flex-col gap-4">
-                        <div className="flex size-16 items-center justify-center rounded-full bg-red-900/60 p-6 transition-transform hover:scale-105">1.</div>
-                        <div className="flex size-16 items-center justify-center rounded-full bg-orange-900/60 p-6 transition-transform hover:scale-105">2.</div>
-                        <div className="flex size-16 items-center justify-center rounded-full bg-yellow-900/60 p-6 transition-transform hover:scale-105">3.</div>
-                        <div className="flex size-16 items-center justify-center rounded-full bg-green-900/60 p-6 transition-transform hover:scale-105">4.</div>
-                        <div className="flex size-16 items-center justify-center rounded-full bg-blue-900/60 p-6 transition-transform hover:scale-105">5.</div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-6 gap-4 p-4">
-                {galleryData.data.map((message) => (
-                    <div
-                        key={message.id}
-                        className="flex flex-col justify-between gap-4 rounded border border-white/30 p-2"
-                    >
-                        {message.attachments?.length > 0 && (
-                            <div>
-                                {message.attachments?.filter((attachment, index) => index < 1)?.map((attachment) => (
-                                    <div>
-                                        <img
-                                            src={attachment.url}
-                                            alt=""
-                                        />
-                                    </div>
-                                ))}
-                                {message.attachments.length > 1 && (
-                                    <div className="mt-2 text-xs font-medium text-white/60">
-                                        +
-                                        {message.attachments.length - 1}
-                                        {' '}
-                                        more
-                                    </div>
-                                )}
-                            </div>
-                        )}
-                        {message.author && (
-                            <div className="flex items-center justify-between gap-2 text-sm font-medium">
-                                <div className="flex items-center gap-2 text-white/90">
-                                    <Image
-                                        src={message.author.avatarURL}
-                                        alt={message.author.username}
-                                        height={24}
-                                        width={24}
-                                        className="rounded-full"
-                                    />
-                                    {message.author.globalName}
-                                </div>
-                                <div className="text-xs text-white/60">
-                                    {moment(message.createdTimestamp).fromNow()}
-                                </div>
-                            </div>
-                        )}
-                    </div>
-
-                ))}
-            </div>
-        </Layout>
-    );
+    return <GalleryPage galleryData={galleryData} />;
 }
