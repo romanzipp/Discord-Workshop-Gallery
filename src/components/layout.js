b36cab975590eefd577ec9f6ff0596fdb8a136b6
Roman Zipp <ich@ich.wtf>
2024-02-10 11:03:21 +0100

    Add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -101,6 +101,37 @@ function Layout({ centered, children }) {
             <article className="relative">
                 {inner}
             </article>
+
+            <footer className="mt-32 pb-8">
+                <div className="mx-auto max-w-4xl border-t border-white/20 px-6 pt-6 text-center">
+                    <div className="flex justify-center gap-6 text-xs text-white/60">
+                        <Link href="/">
+                            Reset server & channel
+                        </Link>
+                        <Link
+                            href="https://romanzipp.com"
+                            target="_blank"
+                        >
+                            Made by @romanzipp
+                        </Link>
+                        <Link href="https://github.com/romanzipp/Discord-Workshop-Gallery">
+                            Open Source on Github
+                        </Link>
+                        <Link
+                            href="https://romanzipp.com/imprint"
+                            target="_blank"
+                        >
+                            Imprint
+                        </Link>
+                        <Link
+                            href="https://romanzipp.com/privacy"
+                            target="_blank"
+                        >
+                            Privacy
+                        </Link>
+                    </div>
+                </div>
+            </footer>
         </main>
     );
 }
