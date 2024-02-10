9df2ea2d9667a198f1ac4aab623e68047aa8cc0c
Roman Zipp <ich@ich.wtf>
2024-02-10 11:41:46 +0100

    Update credits

--- src/components/layout.js
+++ src/components/layout.js
@@ -94,7 +94,7 @@ function Layout({ centered, children }) {
                         </>
                     )}
                     <Link
-                        href="https://romanzipp.com"
+                        href="https://www.instagram.com/romanzipp/"
                         target="_blank"
                         className="flex items-center text-sm font-medium transition-colors hover:text-gray-200"
                     >
@@ -109,17 +109,29 @@ function Layout({ centered, children }) {
 
             <footer className="mt-32 pb-8">
                 <div className="mx-auto max-w-4xl border-t border-white/20 px-6 pt-6 text-center">
-                    <div className="flex justify-center gap-6 text-xs text-white/60">
-                        <Link href="/">
-                            Reset server & channel
-                        </Link>
+                    <div className="flex flex-wrap justify-center gap-6 text-xs text-white/60">
                         <Link
                             href="https://romanzipp.com"
                             target="_blank"
                         >
-                            Made by @romanzipp
+                            ❤️
+                            Made by
+                            {' '}
+                            <span className="font-semibold">Roman Zipp</span>
                         </Link>
-                        <Link href="https://github.com/romanzipp/Discord-Workshop-Gallery">
+                        <Link
+                            href="https://www.instagram.com/romanzipp/"
+                            target="_blank"
+                        >
+                            📸
+                            Instagram
+                            {' '}
+                            <span className="font-semibold">@romanzipp</span>
+                        </Link>
+                        <Link
+                            href="https://github.com/romanzipp/Discord-Workshop-Gallery"
+                            target="_blank"
+                        >
                             Open Source on Github
                         </Link>
                         <Link
@@ -133,6 +145,9 @@ function Layout({ centered, children }) {
                             target="_blank"
                         >
                             Privacy
+                        </Link>
+                        <Link href="/">
+                            Reset server & channel
                         </Link>
                     </div>
                 </div>
