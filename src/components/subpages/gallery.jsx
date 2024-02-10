9927d14f744d4da1cf4150593b36093edede7b3d
Roman Zipp <ich@ich.wtf>
2024-02-10 12:01:59 +0100

    Move message details to own component

--- src/components/subpages/gallery.jsx
+++ src/components/subpages/gallery.jsx
@@ -19,6 +19,7 @@ import {
     Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext,
 } from '@/components/ui/carousel';
 import { Button } from '@/components/ui/button';
+import MessageDetails from '@/components/message-details';
 
 function findClosestMessage(el) {
     // eslint-disable-next-line no-undef
@@ -386,100 +387,19 @@ function Gallery({ galleryData }) {
             </div>
 
             {selectedMessage && (
-                <AlertDialog
-                    defaultOpen
-                    onOpenChange={(open) => !open && setSelectedMessage(null)}
-                >
-                    <AlertDialogTrigger>Open</AlertDialogTrigger>
-                    <AlertDialogContent className={classNames(alertStyles.alert, 'my-4 flex flex-col')}>
-                        <AlertDialogHeader>
-                            <AlertDialogTitle className="flex justify-center">
-                                <UserPreview author={selectedMessage.author} />
-                            </AlertDialogTitle>
-                            <div className="text-sm text-muted-foreground">
-                                <div className="mx-auto max-h-48 max-w-4xl overflow-y-auto px-4">
-                                    {selectedMessageContent && (
-                                        <div dangerouslySetInnerHTML={{ __html: selectedMessageContent }} />
-                                    )}
-                                </div>
-                            </div>
-                        </AlertDialogHeader>
-                        <div className="flex grow flex-col px-10">
-                            <Carousel
-                                onInit={(api) => onCarouselInit(api)}
-                                onDestroy={(api) => onCarouselDestroy(api)}
-                                className="grow"
-                            >
-                                <CarouselContent className="h-full">
-                                    {selectedMessage.attachments?.map((attachment) => (
-                                        <Fragment key={attachment.id}>
-                                            <CarouselItem className="relative h-full">
-                                                <a
-                                                    href={attachment.url}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                >
-                                                    <img
-                                                        src={attachment.url}
-                                                        className="absolute size-full object-contain"
-                                                        alt=""
-                                                    />
-                                                </a>
-                                            </CarouselItem>
-                                        </Fragment>
-                                    ))}
-                                </CarouselContent>
-
-                                {selectedMessage.attachments?.length > 1 && (
-                                    <>
-                                        <CarouselPrevious />
-                                        <CarouselNext />
-                                    </>
-                                )}
-                            </Carousel>
-
-                            {selectedMessageAttachments?.length > 1 && (
-                                <div className="mb-2 flex h-24 justify-center gap-4 pt-6">
-                                    {selectedMessageAttachments?.map((attachment, index) => (
-                                        <Fragment key={attachment.id}>
-                                            <button
-                                                onClick={() => onClickAttachmentThumbnail(attachment, index)}
-                                                type="button"
-                                                className="block h-full"
-                                            >
-                                                <img
-                                                    src={attachment.url}
-                                                    className={classNames(
-                                                        attachment.active ? '' : 'opacity-60',
-                                                        'h-full rounded-md object-contain',
-                                                    )}
-                                                    alt=""
-                                                />
-                                            </button>
-                                        </Fragment>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                        <AlertDialogFooter className="gap-4">
-                            <Button
-                                onClick={() => selectMessagePrev()}
-                                disabled={!prevMessage}
-                                className="min-w-24"
-                            >
-                                Previous
-                            </Button>
-                            <AlertDialogCancel>Close</AlertDialogCancel>
-                            <Button
-                                onClick={() => selectMessageNext()}
-                                disabled={!nextMessage}
-                                className="min-w-24"
-                            >
-                                Next
-                            </Button>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialog>
+                <MessageDetails
+                    selectedMessage={selectedMessage}
+                    selectedMessageAttachments={selectedMessageAttachments}
+                    setSelectedMessage={setSelectedMessage}
+                    selectedMessageContent={selectedMessageContent}
+                    onCarouselInit={onCarouselInit}
+                    nextMessage={nextMessage}
+                    prevMessage={prevMessage}
+                    onClickAttachmentThumbnail={onClickAttachmentThumbnail}
+                    onCarouselDestroy={onCarouselDestroy}
+                    selectMessagePrev={selectMessagePrev}
+                    selectMessageNext={selectMessageNext}
+                />
             )}
         </>
     );
