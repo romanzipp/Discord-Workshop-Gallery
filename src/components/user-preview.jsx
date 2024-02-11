d4796aa81065e441c5161b51e390077fa873931d
Roman Zipp <ich@ich.wtf>
2024-02-11 20:18:27 +0100

    Fix null avatar on user preview

--- src/components/user-preview.jsx
+++ src/components/user-preview.jsx
@@ -4,14 +4,16 @@ import Image from 'next/image';
 function UserPreview({ author }) {
     return (
         <div className="flex items-center gap-2 text-white/90">
-            <Image
-                src={author.avatarURL}
-                alt={author.username}
-                height={24}
-                width={24}
-                className="whitespace-nowrap rounded-full"
-            />
-            {author.globalName}
+            {author?.avatarURL && (
+                <Image
+                    src={author.avatarURL}
+                    alt={author.username}
+                    height={24}
+                    width={24}
+                    className="whitespace-nowrap rounded-full"
+                />
+            )}
+            {author?.globalName}
         </div>
     );
 }
